4a9abf489825a1f3705dba7efefdd44d554af4fe
janghyunjun <buzz03312@naver.com>
2024-06-20 02:47:13 +0900

    버전 다운

--- app/src/main/java/com/example/teamproject/viewmodel/Repository.kt
+++ app/src/main/java/com/example/teamproject/viewmodel/Repository.kt
@@ -6,33 +6,6 @@ import kotlinx.coroutines.tasks.await
 
 class Repository(private val table: DatabaseReference) {
 
-//    suspend fun insertUser(userData: UserData) {
-//        table.child(userData.UserId).setValue(userData)
-//    }
-//
-//    suspend fun deleteItem(userData: UserData) {
-//        table.child(userData.UserId.toString()).removeValue()
-//    }
-//
-//    fun initializeUserList(callback: (List<UserData>) -> Unit) {
-//        val listener = object : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                val userList = mutableListOf<UserData>()
-//                for (userSnapshot in snapshot.children) {
-//                    val user = userSnapshot.getValue(UserData::class.java)
-//                    user?.let { userList.add(it) }
-//                }
-//                Log.d("Repository", "User list initialized with ${userList.size} users.")
-//                callback(userList)
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                Log.e("Repository", "Database error: ${error.message}")
-//            }
-//        }
-//        table.addListenerForSingleValueEvent(listener)
-//    }
-
     fun addUser(userData: UserData) {
         table.child("Users").child(userData.UserId).setValue(userData)
     }
